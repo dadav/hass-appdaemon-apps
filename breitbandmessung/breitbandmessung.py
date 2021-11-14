0d5549a3bf4783857dc6164ade3eced5f5894c92
dadav <33197631+dadav@users.noreply.github.com>
2021-11-14 09:48:39 +0100

    add screenshot functionality and put config in yaml

--- breitbandmessung/breitbandmessung.py
+++ breitbandmessung/breitbandmessung.py
@@ -8,19 +8,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from time import sleep
+from pathlib import Path
 
-WAIT_TIMEOUT = 30
-RESULT_TIMEOUT = 90
 SERVICE_URL = "https://www.breitbandmessung.de/test"
 
-#
-# Breitbandmessung App
-#
-# Args:
-#
-
 class Breitbandmessung(hass.Hass):
-
   def initialize(self):
     self.log('Breitbandmessung started')
     self.register_service("breitbandmessung/run_speedtest", self.run_speedtest)
@@ -28,7 +20,7 @@ class Breitbandmessung(hass.Hass):
     self.run_hourly(self.run_speedtest, runtime)
     self.run_speedtest()
 
-  def run_speedtest(self, **kwargs):
+  def run_speedtest(self, *args, **kwargs):
     self.log('Setting up the chromium driver')
     chrome_options = Options()
     chrome_options.add_argument("--headless")
@@ -36,8 +28,8 @@ class Breitbandmessung(hass.Hass):
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     self.driver = webdriver.Chrome(options=chrome_options)
-    self.element_wait = WebDriverWait(self.driver, WAIT_TIMEOUT)
-    self.result_wait = WebDriverWait(self.driver, RESULT_TIMEOUT)
+    self.element_wait = WebDriverWait(self.driver, self.args["interaction_timeout"])
+    self.result_wait = WebDriverWait(self.driver, self.args["speedtest_timeout"])
     self.driver.get(SERVICE_URL)
     element = self.element_wait.until(EC.invisibility_of_element_located((By.CLASS_NAME, 'modal-dialog-centered')))
     element = self.element_wait.until(EC.element_to_be_clickable((By.ID, 'allow-necessary')))
@@ -58,36 +50,44 @@ class Breitbandmessung(hass.Hass):
     self.result_wait.until(EC.presence_of_element_located((By.XPATH, "//h1[text()='Die Browsermessung ist abgeschlossen.']")))
     sleep(5)
     ping = self.driver.find_element(By.XPATH, "//span[@class='title' and text()='Laufzeit']/parent::div/following-sibling::div/span").text
+    ping = int(ping)
     download = self.driver.find_element(By.XPATH, "//span[@class='title' and text()='Download']/parent::div/following-sibling::div/span").text
+    download = float(download.replace(',','.'))
     upload = self.driver.find_element(By.XPATH, "//span[@class='title' and text()='Upload']/parent::div/following-sibling::div/span").text
+    upload = float(upload.replace(',','.'))
     test_id = self.driver.find_element(By.XPATH, "//div/table/tbody/tr[5]/td[3]").text
     self.log(f"TestID: {test_id}\tPing: {ping}\tDownload: {download}\tUpload: {upload}")
-    last_run = datetime.datetime.now().strftime('%d.%m.%Y, %H:%M:%S')
+    execution_time = datetime.datetime.now().strftime('%d.%m.%Y, %H:%M:%S')
+
+    common_informations = {
+      'test_id': test_id,
+      'server': server,
+      'icon': 'mdi:speedometer',
+      'execution_time': execution_time,
+    }
+
+    if self.args["screenshots"] and (
+      self.args["screenshots_threshold"]["download"] >= download or
+      self.args["screenshots_threshold"]["upload"] >= upload or
+      self.args["screenshots_threshold"]["ping"] <= ping):
+      common_informations = {
+        **common_informations,
+        'screenshot_base64': self.driver.get_screenshot_as_base64()
+      }
+
     self.set_state('sensor.breitbandmessung_ping',
-                    state=int(ping),
-                    attributes={
-                    'test_id': test_id,
-                    'server': server,
-                    'execution_time': last_run,
+                    state=ping,
+                    attributes={**common_informations,
                     'unit_of_measurement': 'ms',
-                    'friendly_name': 'Breitbandmessung Ping',
-                    'icon': 'mdi:speedometer'})
+                    'friendly_name': 'Breitbandmessung Ping'})
     self.set_state('sensor.breitbandmessung_download',
-                    state=float(download.replace(',','.')),
-                    attributes={
-                    'test_id': test_id,
-                    'server': server,
-                    'execution_time': last_run,
+                    state=download,
+                    attributes={**common_informations,
                     'unit_of_measurement': 'Mbit/s',
-                    'friendly_name': 'Breitbandmessung Download',
-                    'icon': 'mdi:speedometer'})
+                    'friendly_name': 'Breitbandmessung Download'})
     self.set_state('sensor.breitbandmessung_upload',
-                    state=float(upload.replace(',','.')),
-                    attributes={
-                    'test_id': test_id,
-                    'server': server,
-                    'execution_time': last_run,
+                    state=upload,
+                    attributes={**common_informations,
                     'unit_of_measurement': 'Mbit/s',
-                    'friendly_name': 'Breitbandmessung Upload',
-                    'icon': 'mdi:speedometer'})
+                    'friendly_name': 'Breitbandmessung Upload'})
     self.driver.quit()
