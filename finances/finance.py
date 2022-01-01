9b6fea8ec006067fa7cde7bf9d504a1c2393150c
dadav <33197631+dadav@users.noreply.github.com>
2022-01-01 12:56:32 +0100

    make it working

--- finances/finance.py
+++ finances/finance.py
@@ -11,41 +11,41 @@ def normalize_sym(old_sym):
     return old_sym
 
 class Finance(hass.Hass):
-  def initialize(self):
-    self.log('Finance started')
-    self.last_time = dict()
-    self.register_service("finance/fetch_data", self.fetch_data)
-    self.run_every(self.fetch_data, "now", self.args["interval_in_minutes"] * 60)
-    self.fetch_data()
+    def initialize(self):
+        self.log('Finance started')
+        self.last_time = dict()
+        self.register_service("finance/fetch_data", self.fetch_data)
+        self.run_every(self.fetch_data, "now", self.args["interval_in_minutes"] * 60)
+        self.fetch_data()
 
-  def fetch_data(self, *args, **kwargs):
-    symbols = ' '.join(self.args['symbols'])
-    self.log('Fetching data for the following symbols: %s', symbols)
+    def fetch_data(self, *args, **kwargs):
+        symbols = ' '.join(self.args['symbols'])
+        self.log('Fetching data for the following symbols: %s', symbols)
 
-    tickers = yf.Tickers(symbols)
+        tickers = yf.Tickers(symbols)
 
-    for sym, data in tickers.tickers.items():
-        data = data.info
-        unix_time = data['regularMarketTime']
+        for sym, data in tickers.tickers.items():
+            data = data.info
+            unix_time = data['regularMarketTime']
 
-        if sym in self.last_time and self.last_time[sym] == unix_time:
-            continue
+            if sym in self.last_time and self.last_time[sym] == unix_time:
+                continue
 
-        self.last_time[sym] = unix_time
+            self.last_time[sym] = unix_time
 
-        nsym = normalize_sym(sym)
-        diff = data['regularMarketChange']
-        diff_pcent = data['regularMarketChangePercent'] * 100.0
-        # time = datetime.utcfromtimestamp(unix_time)
+            nsym = normalize_sym(sym)
+            diff = data['regularMarketChange']
+            diff_pcent = data['regularMarketChangePercent'] * 100.0
+            # time = datetime.utcfromtimestamp(unix_time)
 
-        common = {
-            'device_class': 'monetary',
-            'friendly_name': data['shortName'],
-            'state_class': 'measurement',
-            'unit_of_measurement': data['currencySymbol'],
-            'entity_picture': data['logo_url'],
-        }
+            common = {
+                'device_class': 'monetary',
+                'friendly_name': data['shortName'],
+                'state_class': 'measurement',
+                'unit_of_measurement': data['currencySymbol'],
+                'entity_picture': data['logo_url'],
+            }
 
-        self.set_state(f'sensor.finance_{nsym}', state=data['regularMarketPrice'], attributes={**common})
-        self.set_state(f'sensor.finance_{nsym}_diff', state=diff, attributes={**common})
-        self.set_state(f'sensor.finance_{nsym}_diff_percent', state=diff_pcent, attributes={**common, 'unit_of_measurement': '%'})
+            self.set_state(f'sensor.finance_{nsym}', state=data['regularMarketPrice'], attributes={**common})
+            self.set_state(f'sensor.finance_{nsym}_diff', state=diff, attributes={**common})
+            self.set_state(f'sensor.finance_{nsym}_diff_percent', state=diff_pcent, attributes={**common, 'unit_of_measurement': '%'})
